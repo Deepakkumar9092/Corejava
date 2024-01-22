495de0531a0e341820ef5ede71ccdee3eaa73e81
IyumDeepak <115373833+IyumDeepak@users.noreply.github.com>
2024-01-22 18:52:02 +0530

    gfhj

--- helloworld.java/src/helloworld/java/Demo.java
+++ helloworld.java/src/helloworld/java/Demo.java
@@ -2,9 +2,10 @@ package helloworld.java;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Deepak Kumar");
 
 	}
 
