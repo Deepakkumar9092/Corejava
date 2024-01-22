8da5826939a874d606f84c30b11f1dcfba0fb1c7
IyumDeepak <115373833+IyumDeepak@users.noreply.github.com>
2024-01-22 19:12:31 +0530

    commit

--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -1,10 +1,10 @@
-package helloworld.java;
+package helloworld;
 
-public class Deepak {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+		System.out.print("adfslfa");
 	}
 
 }
