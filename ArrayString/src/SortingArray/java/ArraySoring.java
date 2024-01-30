1975a4210b3602aa84d20f5a4244f48db23a997d
IyumDeepak <115373833+IyumDeepak@users.noreply.github.com>
2024-01-30 14:33:36 +0530

    commit

--- ArrayString/src/SortingArray/java/ArraySoring.java
+++ ArrayString/src/SortingArray/java/ArraySoring.java
@@ -6,16 +6,40 @@ public class ArraySoring {
 	//Sorting array using Arrays.sort functions
 		public static void main(String... args) {
 			int arr[]= {43,23,12,54,32};
-			System.out.println("Array Without Sorting");
-			for(int i=0;i<arr.length;i++) {
-				System.out.print(arr[i]+" ");
 			
-		}
-			System.out.println();
-		Arrays.sort(arr);
-		System.out.println("Array After Sorted");
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		}
+//			Arrays.sort(arr);
+//			for(int c : arr)
+//				System.out.print( c +" ");
+//			System.out.println();
+//			System.out.println("min element "+arr[0]);
+//			System.out.println("max element "+arr[arr.length-1]);
+			
+			
+			//find max and min element without using sort 
+			int max = arr[0];
+			int min=arr[0];
+			for(int i=0;i<arr.length;i++) {
+				if(arr[i]>max)
+					max=arr[i];
+				else if(arr[i]<min)
+					min=arr[i];
+			}
+			System.out.println("Min Element: "+min);
+			System.out.println("Max Element:" +max);
+			
+			
+			//using Arrays.sort predefined functions
+			
+//			System.out.println("Array Without Sorting");
+//			for(int i=0;i<arr.length;i++) {
+//				System.out.print(arr[i]+" ");
+//			
+//		}
+//			System.out.println();
+//		Arrays.sort(arr);
+//		System.out.println("Array After Sorted");
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.print(arr[i]+" ");
+//		}
+	}
 }
